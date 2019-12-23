2b3ddac9303d463b25a2e141b68c260b7227d0ef
superpuper32 <pochitalin_m@mail.ru>
2019-12-24 00:46:05 +0300

    Implemented flatpickr plugin

--- src/components/datepicker.vue
+++ src/components/datepicker.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-    <input type="text" calss="form-control" :value="123" />
+    <input id="datepicker" ref="datepicker" type="text" class="form-control" :value="value" />
   </div>
 </template>
 
 <script>
+import flatpickr from 'flatpickr'
+import 'flatpickr/dist/flatpickr.css'
+
 export default {
-  name: 'Datepicker'
+  name: 'Datepicker',
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  },
+  data: () => ({
+    fp: null
+  }),
+  watch: {
+    value: 'updateDatepicker'
+  },
+  mounted() {
+    this.initDatepicker()
+  },
+  beforeDestroy() {
+    if (this.fp) {
+      this.fp.destory()
+    }
+  },
+  methods: {
+    update(newDate) {
+      this.$emit('input', newDate)
+    },
+    initDatepicker() {
+      this.fp = flatpickr(this.$refs.datepicker, {
+        dateFormat: 'd.m.Y',
+        onChange: (_, dateStr) => this.update(dateStr)
+      })
+    },
+    updateDatepicker() {
+      if (this.fp) {
+        this.fp.setDate(this.value)
+      }
+    }
+  }
 }
 </script>
