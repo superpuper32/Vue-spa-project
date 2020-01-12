a8125bb8e4057cf963fe1a6834f0d67631686a06
superpuper32 <pochitalin_m@mail.ru>
2020-01-12 23:47:14 +0300

    added unicorn linter

--- src/router/index.js
+++ src/router/index.js
@@ -3,33 +3,35 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/views/Home.vue')
+  },
+  {
+    path: '/users',
+    component: () => import('@/views/Users.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/phones',
+    component: () => import('@/views/Phones.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/edit/:id',
+    component: () => import('@/views/EditUser.vue')
+  },
+  {
+    path: '/create',
+    component: () => import('@/views/CreateUser.vue')
+  }
+]
+
 const router = new VueRouter({
   mode: 'history',
   linkExactActiveClass: 'active',
-  routes: [
-    {
-      path: '/',
-      component: () => import('@/views/Home.vue')
-    },
-    {
-      path: '/users',
-      component: () => import('@/views/Users.vue'),
-      meta: { requiresAuth: true }
-    },
-    {
-      path: '/phones',
-      component: () => import('@/views/Phones.vue'),
-      meta: { requiresAuth: true }
-    },
-    {
-      path: '/edit/:id',
-      component: () => import('@/views/EditUser.vue')
-    },
-    {
-      path: '/create',
-      component: () => import('@/views/CreateUser.vue')
-    }
-  ]
+  routes
 })
 
 const token = '123'
